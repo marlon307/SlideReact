2fc2b7b2d3b8f2a7f26ec591aef5c5823ff2758f
marlon307 <marlon-ramosb@hotmail.com>
2021-09-07 17:02:00 -0300

    Init Slide

---
+++ components/SPanel.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react'
+import style from './style.module.css'
+
+
+type Props = {
+  children: ReactNode
+}
+
+function SPanel({ children }: Props) {
+  return (
+    <div className={ style.panel }>
+      { children }
+    </div>
+  )
+}
+
+export default SPanel
---
+++ components/Slide.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode, useRef, useState, useCallback, useEffect, RefObject } from 'react'
+import style from './style.module.css'
+type Props = {
+  children: ReactNode
+}
+
+// Example https://codesandbox.io/s/remove-listener-with-callback-u4h22?file=/src/App.js
+
+function Slide({ children }: Props) {
+
+  const slideRef = useRef<HTMLDivElement>();
+  const [startEv, setStartEv] = useState(false)
+  const [positonX, setPositionX] = useState(0)
+
+  const eventMouseMove = useCallback((event) => {
+    setPositionX(event.offsetX)
+  }, [])
+
+  useEffect(() => {
+    const { current } = slideRef;
+
+    if (startEv) current?.addEventListener('mousemove', eventMouseMove)
+    else current?.removeEventListener('mousemove', eventMouseMove)
+
+    return () => {
+      current?.removeEventListener('mousemove', eventMouseMove);
+    }
+  }, [startEv, eventMouseMove]);
+
+  return (
+    <div
+      ref={ slideRef }
+      className={ style.movePanel }
+      onMouseDown={ () => setStartEv(true) }
+      onMouseUp={ () => setStartEv(false) }
+      onMouseLeave={ () => setStartEv(false) }
+    >
+      <div
+        className={ style.slide }
+        style={ { transform: `translateX(${positonX}px)` } }>
+        { children }
+      </div>
+    </div>
+  )
+}
+
+export default Slide
