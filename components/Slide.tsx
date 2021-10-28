d5dbb6da605ff628feba4990faab6bb844152e53
marlon307 <marlon-ramosb@hotmail.com>
2021-10-28 18:11:11 -0300

    Exemple comment mendiun slide

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -11,7 +11,9 @@ type Props = {
   children: ReactNode
 }
 
+
 // Example https://codesandbox.io/s/remove-listener-with-callback-u4h22?file=/src/App.js
+// https://medium.com/tinyso/how-to-create-the-responsive-and-swipeable-carousel-slider-component-in-react-99f433364aa0
 
 function Slide({ children }: Props) {
 
