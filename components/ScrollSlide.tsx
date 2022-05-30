8c8c52abda016aee31ab70608a4a771cb3b3990e
marlon307 <marlon-ramosb@hotmail.com>
2022-05-30 19:29:20 -0300

    Slide scroll btn

--- components/ScrollSlide.tsx
+++ components/ScrollSlide.tsx
@@ -3,83 +3,71 @@ import style from './sstyle.module.scss';
 // https://jsfiddle.net/pjqxrgwu/
 // https://stackoverflow.com/questions/19669786/check-if-element-is-visible-in-dom
 function ScrollSlid({ children }: any) {
-  const [initialPosition, setInitialPosition] = useState(0);
   const [indexPanel, setIndexPanel] = useState(0);
-  const [enventStarted, setEventStarted] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const elements = ref.current!.querySelectorAll('*');
+    const element = ref.current!.querySelector(`#panel-${indexPanel}`)!;
 
-    const observer = new IntersectionObserver(([entries]) => {
-      if (entries.isIntersecting) {
-        setIndexPanel(Number(entries.target.id.replace('panel-', '')));
-      }
-
-      entries.target.scrollIntoView({
-        behavior: 'smooth',
-        block: 'nearest',
-        inline: 'nearest',
-      });
-    }, { threshold: 1 });
-
-    elements.forEach((element) => {
-      observer.observe(element);
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'nearest',
     });
+  }, [indexPanel]);
 
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    const { current } = ref;
-    function startEvent(event: any) {
-      current?.classList.add(style.stop_animation);
-      setInitialPosition(event.layerX);
-      setEventStarted(true);
+  function next() {
+    if (indexPanel < children.length - 1) {
+      setIndexPanel((stateIndex) => stateIndex + 1);
     }
+  }
 
-    current?.addEventListener('mousedown', startEvent);
-
-    function finishEvent() {
-      current?.classList.remove(style.stop_animation);
-      setEventStarted(false);
+  function prev() {
+    if (indexPanel > 0) {
+      setIndexPanel((stateIndex) => stateIndex - 1);
     }
-
-    current?.addEventListener('mouseup', finishEvent);
-    current?.addEventListener('mouseleave', finishEvent);
-
-    function moveEvent(event: any) {
-      if (enventStarted) {
-        const cValue = current?.children[indexPanel].clientWidth!;
-        current?.scrollTo((initialPosition + (indexPanel * cValue)) - event.layerX, 0);
-      }
-    }
-
-    current?.addEventListener('mousemove', moveEvent);
-    return () => {
-      current?.removeEventListener('mousedown', startEvent);
-      current?.removeEventListener('mousemove', moveEvent);
-      current?.removeEventListener('mouseup', finishEvent);
-      current?.removeEventListener('mouseleave', finishEvent);
-    };
-  }, [enventStarted]);
+  }
 
   return (
-    <div
-      ref={ ref }
-      className={ style.mainpanel }
-    >
-      { children.map((child: any, index: number) => (
-        <div
-          className={ style.content }
-          key={ child.props.children }
-          id={ `panel-${index}` }
-        >
-          { child }
-        </div>
-      )) }
+    <div className={ style.panelsliders }>
+      <button
+        className={ style.next }
+        onClick={ next }
+        type="button"
+        aria-label="Next"
+        data-active={ indexPanel < children.length - 1 }
+      />
+      <div
+        ref={ ref }
+        className={ style.mainpanel }
+      >
+        { children.map((child: any, index: number) => (
+          <div
+            className={ style.content }
+            key={ child.props.children }
+            id={ `panel-${index}` }
+          >
+            { child }
+          </div>
+        )) }
+      </div>
+      <div className={ style.slidecontrols }>
+        { children.map((_: any, index: number) => (
+          <span
+            aria-hidden="true"
+            onClick={ () => setIndexPanel(index) }
+            aria-label="control"
+            data-active={ index === indexPanel }
+          />
+        )) }
+      </div>
+      <button
+        className={ style.prev }
+        onClick={ prev }
+        type="button"
+        aria-label="Prev"
+        data-active={ indexPanel > 0 }
+      />
     </div>
   );
 }
